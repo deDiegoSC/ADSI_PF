c1e6edc02f944497acbf7650f98e4147e800156c
deDiegoSC <u21305475@utp.edu.pe>
2024-07-24 21:14:07 -0500

    Add:Connection DB

--- src/java/Conexion/ConexionDB.java
+++ src/java/Conexion/ConexionDB.java
@@ -4,10 +4,27 @@
  */
 package Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author DIEGO
  */
 public class ConexionDB {
-    
+
+    private static final String URL = "jdbc:mysql://localhost:3306/APR_PF";
+    private static final String USUARIO = "root";
+    private static final String CONTRASEÑA = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Controlador JDBC no encontrado");
+        }
+    }
+
 }
